0df67fd7cd984bcf7780009e091643edde910de1
karol71927 <karolkawalec99@gmail.com>
2022-03-17 15:36:42 +0100

    db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { LoggerMiddleware } from './logger-middleware/logger-middleware';
 import { ProductsController } from './products/products.controller';
-import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [DatabaseModule, ProductsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
