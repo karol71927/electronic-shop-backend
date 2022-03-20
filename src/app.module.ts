db8c2417b65b1c0cdffc21280129304b3bc60629
dazaiii <kin.baryczka@gmail.com>
2022-03-20 20:14:37 +0100

    Authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ProductsModule } from './products/products.module';
 import { LoggerMiddleware } from './logger-middleware/logger-middleware';
 import { ProductsController } from './products/products.controller';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ProductsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      id: 1,
+      username: 'john',
+      password: 'password',
+      email: 'john@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'diana',
+      password: 'pswd',
+      email: 'diana@gmail.com',
+    },
+  ];
+
+  //curl -X POST http://localhost:3000/api/auth/login -d '{"username": "john", "password": "password", "email": "john@gmail.com"}' -H "Content-Type: application/json"
+
+  async findOne(username: string): Promise<User> {
+    return this.users.find((user) => (user.username = username));
+  }
+}
