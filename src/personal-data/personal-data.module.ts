e13a0fbff95e3b49136715ad17a13ed145912cfb
karol71927 <karolkawalec99@gmail.com>
2022-04-02 18:13:37 +0200

    modules fixes

--- src/personal-data/personal-data.module.ts
+++ src/personal-data/personal-data.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PersonalDataController } from './personal-data.controller';
+import { PersonalData } from './personal-data.entity';
+import { PersonalDataService } from './personal-data.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([PersonalData])],
+  controllers: [PersonalDataController],
+  providers: [PersonalDataService],
+})
 export class PersonalDataModule {}
