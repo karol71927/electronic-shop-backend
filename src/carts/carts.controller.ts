fe17473c16ab286b4b733a23b7a77c19cde9c175
karol71927 <karolkawalec99@gmail.com>
2022-05-06 11:13:54 +0200

    cart

--- src/carts/carts.controller.ts
+++ src/carts/carts.controller.ts
@@ -7,11 +7,14 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Cart } from './carts.entity';
 import { CartsService } from './carts.service';
 import { CreateCartDto } from './dto/create-cart.dto';
+import { GetCartDto, GetCartWithTotalPriceDto } from './dto/get-cart.dto';
 
 @Controller('carts')
+@ApiTags('carts')
 export class CartsController {
   constructor(private cartsService: CartsService) {}
 
@@ -23,6 +26,18 @@ export class CartsController {
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<Cart> {
     return this.cartsService.findOne(id);
+  }
+
+  @Get('/user/:id')
+  async findAllForUser(
+    @Param('id') id: number,
+  ): Promise<GetCartWithTotalPriceDto> {
+    const raw = await this.cartsService.findAllForUser(id);
+    const dto: GetCartDto[] = [];
+    raw.forEach((element) => {
+      dto.push(GetCartDto.convertRawToDto(element));
+    });
+    return this.cartsService.calculateTotalPrice(dto);
   }
 
   @Post()
--- src/carts/dto/create-cart.dto.ts
+++ src/carts/dto/create-cart.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCartDto {
+  @ApiProperty({
+    required: false,
+  })
   userId: number;
+  @ApiProperty()
   productId: number;
+  @ApiProperty()
   amount: number;
 }
